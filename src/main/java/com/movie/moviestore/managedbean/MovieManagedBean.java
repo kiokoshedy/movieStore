c444f7dbdb0aa50ac28f9ed2e72440c65600cb89
Shady Kioko <shadrack@dataintegrated.co.ke>
2020-04-24 14:29:48 +0300

    downloading function (pdf and csv)

--- src/main/java/com/movie/moviestore/managedbean/MovieManagedBean.java
+++ src/main/java/com/movie/moviestore/managedbean/MovieManagedBean.java
@@ -5,14 +5,28 @@
  */
 package com.movie.moviestore.managedbean;
 
+import com.lowagie.text.BadElementException;
+import com.lowagie.text.DocumentException;
 import com.movie.moviestore.entity.Movie;
 import com.movie.moviestore.sessionbean.MovieSessionBean;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.Calendar;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.Dependent;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.swing.text.Document;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.primefaces.component.export.ExcelOptions;
+import org.primefaces.component.export.PDFOptions;
 
 
 
@@ -27,6 +41,12 @@ public class MovieManagedBean implements Serializable{
     private List<Movie> movielist;
     @EJB
     private MovieSessionBean msb;
+    
+    private Integer year;
+    
+    private ExcelOptions excelOpt;
+
+    private PDFOptions pdfOpt;
     /**
      * Creates a new instance of MovieManagedBean
      */
@@ -36,7 +56,10 @@ public class MovieManagedBean implements Serializable{
      
     @PostConstruct
     public void init(){
+        Calendar cal = Calendar.getInstance();
         setMovielist(msb.getAllMovies());
+        setYear(cal.get(Calendar.YEAR));
+        customizationOptions();
     }
     public List<Movie> getMovielist() {
         return movielist;
@@ -44,6 +67,68 @@ public class MovieManagedBean implements Serializable{
 
     public void setMovielist(List<Movie> movielist) {
         this.movielist = movielist;
+    }
+    
+    public void customizationOptions() {
+        excelOpt = new ExcelOptions();
+        excelOpt.setFacetBgColor("#F88017");
+        excelOpt.setFacetFontSize("10");
+        excelOpt.setFacetFontColor("#0000ff");
+        excelOpt.setFacetFontStyle("BOLD");
+        excelOpt.setCellFontColor("#00ff00");
+        excelOpt.setCellFontSize("8");
+
+        pdfOpt = new PDFOptions();
+        pdfOpt.setFacetBgColor("#F88017");
+        pdfOpt.setFacetFontColor("#0000ff");
+        pdfOpt.setFacetFontStyle("BOLD");
+        pdfOpt.setCellFontSize("12");
+
+    }
+
+    public void postProcessXLS(Object document) {
+        HSSFWorkbook wb = (HSSFWorkbook) document;
+        HSSFSheet sheet = wb.getSheetAt(0);
+        HSSFRow header = sheet.getRow(0);
+        
+        HSSFCellStyle cellStyle = wb.createCellStyle();
+
+        for (int i = 0; i < header.getPhysicalNumberOfCells(); i++) {
+            HSSFCell cell = header.getCell(i);
+
+            cell.setCellStyle(cellStyle);
+        }
+    }
+
+    public void preProcessPDF(Object document) throws IOException, BadElementException, DocumentException {
+        Document pdf = (Document) document;
+       
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+    }
+
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public ExcelOptions getExcelOpt() {
+        return excelOpt;
+    }
+
+    public void setExcelOpt(ExcelOptions excelOpt) {
+        this.excelOpt = excelOpt;
+    }
+
+    public PDFOptions getPdfOpt() {
+        return pdfOpt;
+    }
+
+    public void setPdfOpt(PDFOptions pdfOpt) {
+        this.pdfOpt = pdfOpt;
     }
     
 }
